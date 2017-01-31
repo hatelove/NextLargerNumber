0aa8c8eec888f664b11e79d9bc9145077021929a
hatelove <jay.hatelove@gmail.com>
2017-01-31 11:32:32 +0800

    input equals 1 should return -1

--- NextLargerNumber/NextLargerNumberTests.cs
+++ NextLargerNumber/NextLargerNumberTests.cs
@@ -9,7 +9,22 @@ namespace NextLargerNumber
         [TestMethod]
         public void Test_1_should_be_minus_1()
         {
-            
+            var input = 1;
+
+            var expected = -1;
+
+            int actual = NextLargerNumber.Next(input);
+
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+
+    public static class NextLargerNumber
+    {
+        internal static int Next(int input)
+        {
+            throw new NotImplementedException();
         }
     }
 }

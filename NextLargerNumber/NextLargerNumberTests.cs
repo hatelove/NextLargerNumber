6f0cecd67f919e734ce431bec0958f6184359669
hatelove <jay.hatelove@gmail.com>
2017-01-31 11:28:19 +0800

    initialized

---
+++ NextLargerNumber/NextLargerNumberTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NextLargerNumber
+{
+    [TestClass]
+    public class NextLargerNumberTests
+    {
+        [TestMethod]
+        public void Test_1_should_be_minus_1()
+        {
+            
+        }
+    }
+}
